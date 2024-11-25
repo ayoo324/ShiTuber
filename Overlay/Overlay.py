74afd78cadcba5375ea830fadee78f3adef68d43
ayoo324 <ibr1362@gmail.com>
2024-11-25 18:32:34 -0500

    made x and y movement separate

--- Overlay/Overlay.py
+++ Overlay/Overlay.py
@@ -12,8 +12,9 @@ CHUNK = 8  # Samples: 1024,  512, 256, 128 frames per buffer
 RATE = 44100  # Equivalent to Human Hearing at 40 kHz
 CHANNELS = 1
 AUDIO_THRESHOLD = 50
-MAX_MOVEMENT = 20
-flipCooldown = 300
+MAX_MOVEMENT_X = 20
+MAX_MOVEMENT_Y = 40
+flipCooldown = 0.15
 class Overlay:
     def __init__(self):
         self.dom = DOM()
@@ -48,26 +49,28 @@ class Overlay:
         self.dom.focus.press(key)
     sign = -1
     last_average_of_data = 0
-    cur_sum = 0
     last_sum = 0
-    head_speed_up = 3
-    head_speed_down = 5
+    horizontal_movement = 0
+    head_speed_up = 5
+    head_speed_down = 3
+    head_speed_horizontal = 3
     lastFlipTime = time.time()
     def handleAudioData(self, data):
         average_of_data = int(abs(sum(data) / CHUNK))
-        self.cur_sum = self.last_average_of_data 
+        cur_sum = self.last_sum
+
         if average_of_data > AUDIO_THRESHOLD:
             
             self.audioBar.dimensions = (20, int(average_of_data))
             if self.last_average_of_data > average_of_data:
-                self.cur_sum = self.last_sum - self.head_speed_down
+                cur_sum = self.last_sum - self.head_speed_down
             if self.last_average_of_data < average_of_data:
-                self.cur_sum = self.last_sum + self.head_speed_up
+                cur_sum = self.last_sum + self.head_speed_up
 
             now = time.time()
-            if self.lastFlipTime + flipCooldown > now:
+            if self.lastFlipTime + flipCooldown < now:
                 self.lastFlipTime = now
-                if random.randint(0, 5) == 1:
+                if random.randint(0, 3) == 1:
                     self.sign = self.sign * -1
 
             self.last_average_of_data = average_of_data
@@ -75,9 +78,36 @@ class Overlay:
             cur_sum = self.last_sum - self.head_speed_down  
             self.last_average_of_data = cur_sum
 
-        if self.cur_sum < 0:
-            self.cur_sum = 0
-        elif self.cur_sum > MAX_MOVEMENT:
-            self.cur_sum = MAX_MOVEMENT
-        self.last_sum = self.cur_sum
-        self.head.pos = (760 + int(self.cur_sum * self.sign), max(0, 640 - int(self.cur_sum)))
+
+        if cur_sum < 0:
+            cur_sum = 0
+
+        cur_horizontal_movement = self.horizontal_movement
+        if average_of_data > AUDIO_THRESHOLD:
+
+            if self.sign >= 0:
+                cur_horizontal_movement = cur_horizontal_movement + self.head_speed_horizontal
+            else:
+                cur_horizontal_movement = cur_horizontal_movement - self.head_speed_horizontal
+
+            if cur_horizontal_movement < -MAX_MOVEMENT_X:
+                cur_horizontal_movement = -MAX_MOVEMENT_X
+            if cur_horizontal_movement > MAX_MOVEMENT_X:
+                cur_horizontal_movement = MAX_MOVEMENT_X
+        else:
+            if cur_horizontal_movement < 0:
+                cur_horizontal_movement = cur_horizontal_movement + self.head_speed_horizontal
+                if cur_horizontal_movement > 0:
+                    cur_horizontal_movement = 0
+            else:
+                cur_horizontal_movement = cur_horizontal_movement - self.head_speed_horizontal
+                if cur_horizontal_movement < 0:
+                    cur_horizontal_movement = 0
+
+
+        self.horizontal_movement = min(cur_horizontal_movement, MAX_MOVEMENT_X)
+        moveX = self.horizontal_movement 
+        moveY = min(cur_sum, MAX_MOVEMENT_Y)
+
+        self.last_sum = cur_sum = min(cur_sum, MAX_MOVEMENT_Y)
+        self.head.pos = (760 + int(moveX), max(0, 640 - int(moveY)))
